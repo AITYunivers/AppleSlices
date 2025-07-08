61e28dfac05fc7811bd4666a0c3d9e8b0718563e
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2025-07-08 02:05:57 -0700

    Update Icon, remove StationAPI dependency

--- src/main/java/io/github/yunivers/appleslices/mixin/WorldMixin.java
+++ src/main/java/io/github/yunivers/appleslices/mixin/WorldMixin.java
@@ -2,8 +2,8 @@ package io.github.yunivers.appleslices.mixin;
 
 import io.github.yunivers.appleslices.AppleSlices;
 import net.minecraft.world.World;
-import net.modificationstation.stationapi.api.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,6 +22,16 @@ public class WorldMixin
             AppleSlices.pulseDir = -1;
         if (AppleSlices.pulseUnclamped <= -0.5f)
             AppleSlices.pulseDir = 1;
-        AppleSlices.pulseValue = MathHelper.clamp(AppleSlices.pulseUnclamped, 0, 1) * AppleSlices.maxAlpha;
+        AppleSlices.pulseValue = clamp(AppleSlices.pulseUnclamped, 0, 1) * AppleSlices.maxAlpha;
+    }
+
+    // Removing StationAPI dependency
+    @SuppressWarnings("SameParameterValue")
+    @Unique
+    private float clamp(float value, float min, float max)
+    {
+        if (value > max)
+            return max;
+        return Math.max(value, min);
     }
 }
